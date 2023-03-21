ef30611b842c4ab57e83d84ad70d4668abe04ead
Gabriel <gabe.jara@gmail.com>
2023-03-21 09:35:22 +0100

    Blå

--- src/User.java
+++ src/User.java
@@ -3,11 +3,48 @@ public class User {
     private String password;
 
     public User(String username, String password) {
+        setUsername(username);
+        setPassword(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty or null.");
+        }
         this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be empty or null.");
+        }
         this.password = password;
     }
 
-    public boolean equals(User otherUser) {
-        return this.username.equals(otherUser.username) && this.password.equals(otherUser.password);
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User otherUser = (User) obj;
+        return username.equals(otherUser.username) && password.equals(otherUser.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
     }
 }
